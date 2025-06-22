ae4a69b02768bf56df55546d3fdd88d4107b6df5
joe-parker-naber <joenaber@hotmail.co.uk>
2025-06-22 23:36:10 +0100

    test: reduce flake, make less brittle

--- tests/features/add_employee.spec.ts
+++ tests/features/add_employee.spec.ts
@@ -2,9 +2,17 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/login";
 import { DashboardPage } from "../pages/dashboard";
 import { EmployeeHubPage } from "../pages/emplyee-hub";
+import { v4 as uuidv4 } from "uuid";
 
 const username = process.env.BRIGHTHR_USER_ID as string;
 const password = process.env.BRIGHTHR_USER_PASSWORD as string;
+let employee = {
+  firstName: uuidv4(),
+  lastName: "User",
+  email: "test@email.com",
+  phoneNumber: "1234567890",
+  jobTitle: "Test Job",
+};
 
 test.describe("Employees hub", () => {
   test("A logged in user can add an new employee via the employees hub", async ({
@@ -22,14 +30,14 @@ test.describe("Employees hub", () => {
     await dashboardPage.visitNavLink();
     const employeeHub = new EmployeeHubPage(page);
     await expect(employeeHub.pageTitle).toBeVisible();
-    await employeeHub.addEmployee();
+    await employeeHub.addEmployee(employee);
     await expect(employeeHub.pageTitle).toBeVisible();
     await expect(employeeHub.employeeListTitle).toBeVisible();
-    await expect(employeeHub.employeeListTitle).toContainText("(2)");
+    await employeeHub.findEmployee(employee.firstName);
   });
 });
 test.afterEach(async ({ page }) => {
   const employeeHub = new EmployeeHubPage(page);
-  await employeeHub.deleteEmployee();
+  await employeeHub.deleteEmployee(employee.firstName);
   await expect(employeeHub.pageTitle).toBeVisible();
 });
